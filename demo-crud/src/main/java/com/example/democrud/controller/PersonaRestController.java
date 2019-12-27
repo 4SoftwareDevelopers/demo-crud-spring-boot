2ab98b44f696a79fe98839326b492e6f69515589
Jordy Rodríguez <jrodriguez@presteamshop.com>
2019-12-26 20:33:18 -0500

    Ajuste controlador y gitignore
    Cambios en gitignore y ajuste a controlador rest, se añade la anotación @CrossOrigin para poder hacer peticiones HTTP desde aplicaciones externas.

--- demo-crud/src/main/java/com/example/democrud/controller/PersonaRestController.java
+++ demo-crud/src/main/java/com/example/democrud/controller/PersonaRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import com.example.democrud.service.api.PersonaServiceAPI;
 
 @RestController
 @RequestMapping(value = "/api/v1/")
+@CrossOrigin("*")
 public class PersonaRestController {
 
 	@Autowired
